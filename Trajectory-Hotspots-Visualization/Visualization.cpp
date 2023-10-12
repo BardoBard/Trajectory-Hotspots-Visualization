cc70d60b5b3307f5bfc1611d082fed3d88cf2ebd
Bard <nigel.terpstra@student.nhlstenden.com>
2023-10-12 11:01:18 +0200

    moved files

---
+++ Trajectory-Hotspots-Visualization/Visualization.cpp
@@ -0,0 +1,13 @@
+#include "window.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    QWidget* x = app.activeWindow();
+    const std::string window_name = "test";
+    std::vector<Vec2> vecs = { Vec2(0,1), Vec2(1,0), Vec2(0,-1), Vec2(-1,0) };
+    window* w = new window(x, vecs, window_name.c_str());
+
+    w->show();
+    return app.exec();
+}
---
+++ Trajectory-Hotspots-Visualization/pch.h
@@ -0,0 +1,10 @@
+//
+// Created by Bardio - NHL_STENDEN
+//
+#pragma once
+
+#include <string>
+#include <vector>
+#include <CGAL/Qt/Basic_viewer_qt.h>
+#include <Trajectory_Hotspots/pch.h>
+#include <Trajectory_Hotspots/vec2.h>
